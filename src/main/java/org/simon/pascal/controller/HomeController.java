7548ea0bcf51134ae8e1d4335d7b426bd2422e3c
simon.pascal.ngos <simon.pascal.ngos@MAE-DTP-JB36.dir.svc.accenture.com>
2016-05-16 10:41:16 +0400

    angularjs

--- src/main/java/org/simon/pascal/controller/HomeController.java
+++ src/main/java/org/simon/pascal/controller/HomeController.java
@@ -1,9 +1,5 @@
 package org.simon.pascal.controller;
 
-import org.simon.pascal.domains.CalcInput;
-import org.simon.pascal.domains.Result;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,9 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @RequestMapping("/")
+    public String index(){
+        return "redirect:/clock.htm";
+    }
 
-    @RequestMapping("/echo.html")
+    @RequestMapping("/clock.htm")
     public String start(){
-        return "echo";
+        return "clock";
     }
 }
